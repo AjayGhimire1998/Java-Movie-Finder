2f42a87446aa9916108af9cb12ae1b600aae6542
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-07-29 19:29:30 +1000

    movie

--- src/main/webapp/moviejsp/MovieIO.java
+++ src/main/webapp/moviejsp/MovieIO.java
@@ -2,9 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.File;
 import java.util.ArrayList;
-import org.apache.tomcat.jni.File;
 
 public class MovieIO {
   public static ArrayList<Movie> getMovies() {
@@ -19,10 +19,30 @@ public class MovieIO {
       File file = new File(name);
       in = new BufferedReader(new FileReader(file));
     } catch (FileNotFoundException e) {
-      System.out.println("File doesn't exist!");
+      System.err.println("File doesn't exist!");
+      System.exit(0);
     }
+
+    return in;
   }
 
+  private static Movie readMovie(BufferedReader in) {
+    String line = "";
+
+    try {
+      line = in.readLine();
+    } catch (IOException e) {
+      System.err.println("IO error occured!");
+    }
+
+    if (line == null) {
+      return null;
+    } else {
+      String[] data = line.split("\t");
+      return new Movie(data[0], Integer.parseInt(data[1]), Double.parseDouble(data[2]));
+    }
+
+  }
 
 
 }
