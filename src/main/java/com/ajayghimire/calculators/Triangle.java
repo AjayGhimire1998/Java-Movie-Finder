5853091d9bddafe5ad47c5ecbc1acb158798adc8
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-07-29 20:56:58 +1000

    side C

---
+++ src/main/java/com/ajayghimire/calculators/Triangle.java
@@ -0,0 +1,40 @@
+package com.ajayghimire.calculators;
+
+public class Triangle {
+
+  private double sideA;
+  private double sideB;
+
+
+  public Triangle() {
+    this.sideA = 0.0;
+    this.sideB = 0.0;
+  }
+
+  public String getSideA() {
+    return Double.toString(sideA);
+  }
+
+  public void setSideA(String sideA) {
+    try {
+      this.sideA = Double.parseDouble(sideA);
+    } catch (Exception e) {
+      this.sideA = 0.0;
+    }
+
+  }
+
+  public String getSideB() {
+    return Double.toString(sideB);
+  }
+
+  public void setSideB(String sideB) {
+    try {
+      this.sideB = Double.parseDouble(sideB);
+    } catch (Exception e) {
+      this.sideB = 0.0;
+    }
+
+  }
+
+}
